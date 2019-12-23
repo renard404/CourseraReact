396a59ebbe60639a03ab67951c01e5ffa4dfdef9
Komal_Bhale <komalbhale007@gmail.com>
2019-12-23 22:16:04 +0530

    added action creator.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-
+import { addComment } from "../redux/ActionCreators";
 const mapStateToProps = state => {
     return {
         dishes: state.dishes,
@@ -17,6 +17,9 @@ const mapStateToProps = state => {
         leaders: state.leaders
     }
 }
+const MapDispatchToProps = (dispatch) => ({
+    addComment: (dishId, rating, author, comment) => dispatch(addComment((dishId, rating, author, comment)))
+});
 class Main extends Component {
 
     constructor(props) {
@@ -43,7 +46,9 @@ class Main extends Component {
     DishWithId = ({ match }) => {
         return (
             <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                addComment={this.props.addComment}
+            />
         );
     };
 
@@ -70,4 +75,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, MapDispatchToProps)(Main));
