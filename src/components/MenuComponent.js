e0c8e6fd47add1052295976a80269591ca9394d9
Komal_Bhale <komalbhale007@gmail.com>
2019-12-27 22:47:38 +0530

    Fetch from Server. cross-fetch

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 class Menu extends Component {
   constructor(props) {
@@ -37,12 +38,12 @@ class Menu extends Component {
           </div>
         )
       }
-      else
+      else {
         return (
           <div className="col-12 col-md-5 m-1">
             <Link to={`/menu/${dish.id}`}>
               <Card key={dish.id} onClick={() => { this.onDishSelect(dish) }}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardImgOverlay>
                   <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
@@ -50,10 +51,11 @@ class Menu extends Component {
             </Link>
           </div>
         );
+      }
     });
 
     return (
-      <div className="container">
+      <div className="container" >
         <div className="row">
           <Breadcrumb>
             <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
